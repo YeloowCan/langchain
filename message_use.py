c74f9c3c86e94f23adfd5a0c41c53d53d57b5e10
YeloowCan <942789838@qq.com>
2025-10-26 12:19:17 +0800

    refactor(message_use.py): 替换为使用ChatOpenAI并添加环境变量配置

--- message_use.py
+++ message_use.py
@@ -1,13 +1,22 @@
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
-from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+model = ChatOpenAI(
+  api_key=os.getenv("DEEPSEEK_API_KEY"),
+  base_url="https://api.deepseek.com",
+  model="deepseek-chat", 
+)
 
 user_message = HumanMessage(content="今天的天气怎么样")
 
 system_message = SystemMessage(content="你是一个幽默的助手")
 
-chat_template = ChatPromptTemplate.from_messages([
-    system_message,
-    user_message,
-])
+messages = [system_message, user_message]
 
-print(user_message)
+response = model.invoke(messages)
+
+print(response)
